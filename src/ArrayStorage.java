0bbb3aa75ce646e84e5f9f0b99c06ca2d5ec3558
Vitaly <vital.gavrilov1983@gmail.com>
2021-09-12 19:36:16 +0300

    First commit

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,10 +5,25 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+
     }
 
     void save(Resume r) {
+
+        for (int z = 0; z < storage.length - 1; z++) {
+            if (storage[z] == null) {
+                if (r.uuid == null | r.uuid == "0" | r.uuid == "") {
+                    System.out.println("Введите правильный uuid, а не " + r.uuid);
+                    break;
+                }
+                storage[z] = r;
+                System.out.println("storage z= " + storage[z].uuid);
+                break;
+
+            }
+        }
     }
+
 
     Resume get(String uuid) {
         return null;
