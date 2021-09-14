6c25e50c00103be18cc33dd9a9f31afee773b48b
Vitaly <vital.gavrilov1983@gmail.com>
2021-09-14 14:46:26 +0300

    new update basejava

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -7,6 +7,14 @@ public class ArrayStorage {
 
     void clear() {
 
+        // ищем резюме = null и присваиваем их uuid=0- или может я неправильно понял задание?
+        for (int z = 0; z < storage.length; z++) {
+            if (storage[z] == null) {
+                storage[z] = new Resume();
+                storage[z].uuid = "0";
+            }
+        }
+        System.out.println("Количество резюме в БД без null= ");
     }
 
     void save(Resume r) {
@@ -29,26 +37,31 @@ public class ArrayStorage {
 
         for (int z = 0; z < storage.length; z++) {
             if (storage[z] == null) continue;
-            if (storage[z].uuid==uuid) return storage[z];
+            if (storage[z].uuid == uuid) return storage[z];
         }
         return null;
     }
 
     void delete(String uuid) {
 
-   // ищем резюме в базе по String uuid  и перезаписываем его следующим за ним в базе резюме
+        // ищем резюме в базе по String uuid  и перезаписываем его следующим за ним в базе резюме
+        int del_flag=0;  // флаг говорящий об удалении резюме
+
         for (int z = 0; z < storage.length; z++) {
             if (storage[z] == null) continue;
-            if (storage[z].uuid==uuid) {
-                if (z==storage.length-1)  {
-                    storage[z]=null;
+            if (storage[z].uuid == uuid) {
+                del_flag=1;
+                if (z == storage.length - 1) {
+                    storage[z] = null;
+                    System.out.println("Резюме с uuid="+uuid+" удалено");
                     break;
                 }
                 for (int a = z; a < storage.length; a++) {
-                    if ((a+1)<10000) {
+                    if ((a + 1) < 10000) {
                         storage[a] = storage[a + 1];
                     }
                 }
+                System.out.println("Резюме с uuid="+uuid+" удалено");
                 break;
             }
         }
@@ -64,31 +77,33 @@ public class ArrayStorage {
         int res_count = 0;
         for (int z = 0; z < storage.length; z++) {
             if (storage[z] == null) {
-                continue; }
+                continue;
+            }
             res_count = res_count + 1;
         }
         Resume[] res = new Resume[res_count];         // создаем массив резюме
         // ищем резюме - не null и копируем в новый массив res -без null
         int res_index = 0;                // индекс для нового массива res
         for (int a = 0; a < storage.length; a++) {
-            if (storage[a] == null) { continue;}
-            else {
+            if (storage[a] == null) {
+                continue;
+            } else {
                 res[res_index] = storage[a];
                 res_index = res_index + 1;
             }
         }
-     //   System.out.println("Вывод резюме из БД не равных null ");
+        //   System.out.println("Вывод резюме из БД не равных null ");
         return res;
     }
 
     int size() {
         int res_count = 0;
-            for (int z = 0; z < storage.length; z++) {
-                if (storage[z] == null) continue;
-                else res_count = res_count + 1;
-            }
-            System.out.println("Количество резюме в БД без null= ");
-            return res_count;
+        for (int z = 0; z < storage.length; z++) {
+            if (storage[z] == null) continue;
+            else res_count = res_count + 1;
+        }
+        System.out.println("Количество резюме в БД без null= ");
+        return res_count;
     }
 }
 
