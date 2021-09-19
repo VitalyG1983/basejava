2966c1b684c1c55e0e2e90600062c2fd8e8e5d60
Vitaly <vital.gavrilov1983@gmail.com>
2021-09-19 14:48:28 +0300

    new metod for dublication code

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -21,45 +21,40 @@ public class ArrayStorage {
     public void save(Resume r) {
         if (r.getUuid() == null || r.getUuid() == "") {
             System.out.println("Enter valid uuid, not " + r.getUuid());
-        } else if (update(r) == false) {
-            if (size == storage.length) {
-                System.out.println("Not enough space in Data base for save new resume");
-            } else if (size < storage.length) {
-                storage[size] = r;
-                System.out.println("storage[" + size + "].uuid= " + storage[size].getUuid());
-                size++;
-            }
-        } else System.out.println("resume with uuid=" + r.getUuid() + " updated");
+        } else if (searchInd(r, null) >= 0) {
+            System.out.println("Resume with uuid=" + r.getUuid() + " already exist in Database");
+        } else if (size == storage.length) {
+            System.out.println("Not enough space in Data base for save new resume");
+        } else if (size < storage.length) {
+            storage[size] = r;
+            System.out.println("storage[" + size + "].uuid= " + storage[size].getUuid());
+            size++;
+        }
     }
 
+
     public Resume get(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid() == uuid) return storage[i];
+        int ind = searchInd(null, uuid);
+        if (ind >= 0) return storage[ind];
+        else {
+            System.out.println("Resume with uuid= " + uuid + " is not present in Database");
+            return null;
         }
-        System.out.println("Resume with uuid= " + uuid + " is not present in Database");
-        return null;
     }
 
     public void delete(String uuid) {
         // ищем резюме в базе по String uuid  и перезаписываем его следующим за ним в базе резюме
-        int delFlag = 0;
-        for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid() == uuid) {
-                if (i == size - 1) {
-                    storage[i] = null;
-                    delFlag = 1;
-                    break;
-                }
+        int i = searchInd(null, uuid);
+        if (i >= 0) {
+            if (i == size - 1) {
+                storage[i] = null;
+            } else {
                 for (int j = i; j < size; j++) {
                     if ((j + 1) < storage.length) {
                         storage[j] = storage[j + 1];
                     }
                 }
-                delFlag = 1;
-                break;
             }
-        }
-        if (delFlag == 1) {
             size--;
             System.out.println("Resume with uuid=" + uuid + " deleted");
         } else System.out.println("Resume with uuid=" + uuid + " is not present in Database");
@@ -77,14 +72,25 @@ public class ArrayStorage {
         return size;
     }
 
-    public boolean update(Resume resume) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid() == resume.getUuid()) {
-                // resume founded, -> load new resume instead old
-                storage[i] = resume;
-                return true;
+    public void update(Resume resume) {
+        int i = searchInd(resume, null);
+        if (i >= 0) {
+            // resume founded, -> load new resume instead old
+            storage[i] = resume;
+            System.out.println("Resume with uuid=" + resume.getUuid() + " updated in Database");
+        }
+    }
+
+    public int searchInd(Resume resume, String uuid) {
+        if (resume != null) {
+            for (int i = 0; i < size; i++) {
+                if (storage[i].getUuid() == resume.getUuid()) return i;
+            }
+        } else if (uuid != null) {
+            for (int i = 0; i < size; i++) {
+                if (storage[i].getUuid() == uuid) return i;
             }
         }
-        return false;
+        return -1;
     }
 }
