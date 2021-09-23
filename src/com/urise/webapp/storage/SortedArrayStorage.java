8f914cbaad243b75e1d04ec4fd6d6601fbd1c1ae
Vitaliy Gavrilov <vital.gavrilov1983@gmail.com>
2021-09-23 04:49:34 +0300

    Task_03

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -4,35 +4,66 @@ import com.urise.webapp.model.Resume;
 
 import java.util.Arrays;
 
+//import static java.lang.Math.abs;
+
 /**
  * Array based storage for Resumes
  */
 
 public class SortedArrayStorage extends AbstractArrayStorage {
 
-    @Override
     public void clear() {
-
+        // fill 'null' instead real resumes
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
-    @Override
     public void save(Resume r) {
-
+        int index = searchInd(r.getUuid());
+        if (r.getUuid() == null || r.getUuid() == "") {
+            System.out.println("Enter valid uuid, not " + r.getUuid());
+        } else if (index >= 0) {
+            System.out.println("Resume with uuid=" + r.getUuid() + " already exist in Database");
+        } else if (size >= storage.length) {
+            System.out.println("Not enough space in Database for save new resume");
+        } else {//if (size < storage.length)
+            if (index < 0) index = -(index + 1);
+            System.arraycopy(storage, index, storage, index + 1, size - index + 1);
+            storage[index] = r;
+            System.out.println("storage[" + size + "].uuid= " + storage[size].getUuid());
+            size++;
+        }
     }
 
-    @Override
     public void delete(String uuid) {
-
+        // ищем резюме в базе по String uuid  и перезаписываем его следующим за ним в базе резюме
+        int index = searchInd(uuid);
+        if (index >= 0) {
+            if (index == size - 1) {
+                storage[index] = null;
+            } else {
+                System.arraycopy(storage, index + 1, storage, index, size - index + 1);
+                size--;
+                System.out.println("Resume with uuid=" + uuid + " deleted");
+            }
+        } else System.out.println("Resume with uuid=" + uuid + " is not present in Database");
     }
 
-    @Override
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
     public Resume[] getAll() {
-        return new Resume[0];
+        // Create copy of resumes without 'null'
+        return Arrays.copyOf(storage, size);
     }
 
-    @Override
     public void update(Resume resume) {
-
+        int index = searchInd(resume.getUuid());
+        if (index >= 0) {
+            // resume founded, -> save new resume instead old
+            storage[index] = resume;
+            System.out.println("Resume with uuid=" + resume.getUuid() + " updated in Database");
+        } else System.out.println("Resume with uuid=" + resume.getUuid() + " not founded in Database");
     }
 
     @Override
