92047d21aa799edbae2e259e7f500e6a66a98ab1
Vitaliy Gavrilov <vital.gavrilov1983@gmail.com>
2021-10-04 22:35:59 +0300

    ArrayStorageTest super2

--- test/com/urise/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -1,16 +1,20 @@
 package com.urise.webapp.storage;
 
+import com.urise.webapp.exception.StorageException;
 import com.urise.webapp.model.Resume;
 import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class AbstractArrayStorageTest {
-    // private Storage storage = new ArrayStorage();
-    private Storage storage;
+     private Storage storage = new ArrayStorage();
+   // private Storage storage;
     private static final String UUID_1 = "uuid1";
     private static final String UUID_2 = "uuid2";
     private static final String UUID_3 = "uuid3";
+
+    //public AbstractArrayStorageTest() {
+    //}
 
     public AbstractArrayStorageTest(Storage storage) {
         this.storage = storage;
@@ -36,9 +40,17 @@ class AbstractArrayStorageTest {
     }
 
     @Test
-    void save() {
-
-
+    void save()  {
+        try {
+            Resume resume1 = storage.get(UUID_1);
+            Resume resume2 = storage.get(UUID_2);
+            Resume resume3 = storage.get(UUID_3);
+            Assert.assertEquals(UUID_1, resume1.getUuid());
+            Assert.assertEquals(UUID_2, resume2.getUuid());
+            Assert.assertEquals(UUID_3, resume3.getUuid());
+        } catch (StorageException e) {
+            e.getMessage();
+        }
     }
 
     @Test
--- test/com/urise/webapp/storage/ArrayStorageTest.java
+++ test/com/urise/webapp/storage/ArrayStorageTest.java
@@ -13,6 +13,7 @@ class ArrayStorageTest extends AbstractArrayStorageTest {
 
     @Test
     void saveResume() {
+
     }
 
     @Test
