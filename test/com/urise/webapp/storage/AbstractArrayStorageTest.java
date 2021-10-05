4c4d4b28754e6b42cecf0f6c47e3b2a2f06311b1
Виталий Гаврилов <vitalygavrilov@MacBook-Air-Vitalij.local>
2021-10-05 14:55:27 +0300

    AbstractArrayStorageTest main

--- test/com/urise/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -3,6 +3,7 @@ package com.urise.webapp.storage;
 import com.urise.webapp.exception.StorageException;
 import com.urise.webapp.model.Resume;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -24,7 +25,7 @@ class AbstractArrayStorageTest {
     }
 
     @BeforeEach
-    public void setUp() throws Exception {
+    public void setUp() {
         storage.clear();
         storage.save(new Resume(UUID_1));
         storage.save(new Resume(UUID_2));
@@ -43,25 +44,26 @@ class AbstractArrayStorageTest {
     }
 
     @Test
-    void save() throws StorageException{
-        try {
-            Resume resume1 = storage.get(UUID_1);
-            Resume resume2 = storage.get(UUID_2);
-            Resume resume3 = storage.get(UUID_3);
-            Assert.assertEquals(UUID_1, resume1.getUuid().intern());
-            Assert.assertEquals(UUID_2, resume2.getUuid().intern());
-            Assert.assertEquals(UUID_3, resume3.getUuid().intern());
-        } catch (StorageException e) {
-            e.getMessage();
-        }
+    void save() {
+        Resume resume1 = storage.get(UUID_1);
+        Resume resume2 = storage.get(UUID_2);
+        Resume resume3 = storage.get(UUID_3);
+        Assert.assertEquals(UUID_1, resume1.getUuid().intern());
+        Assert.assertEquals(UUID_2, resume2.getUuid().intern());
+        Assert.assertEquals(UUID_3, resume3.getUuid().intern());
     }
 
     @Test
     void delete() {
+        storage.delete(UUID_2);
+        Assert.assertEquals(2, storage.size());
     }
 
     @Test
     void update() {
+        Resume resumeNew = new Resume(UUID_1);
+        storage.update(resumeNew);
+        Assert.assertEquals(resumeNew, storage.get(UUID_1));
     }
 
     @Test
@@ -70,5 +72,20 @@ class AbstractArrayStorageTest {
 
     @Test
     void get() {
+    }
+
+    public static void main(String... args) {
+        AbstractArrayStorageTest abstractArrayStorageTest = new AbstractArrayStorageTest(new ArrayStorage());
+        try {
+            abstractArrayStorageTest.setUp();
+            abstractArrayStorageTest.size();
+        } catch (Exception e) {
+            e.getMessage();
+        }
+        abstractArrayStorageTest.save();
+        abstractArrayStorageTest.delete();
+        abstractArrayStorageTest.update();
+        abstractArrayStorageTest.clear();
+
     }
 }
