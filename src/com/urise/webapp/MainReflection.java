9b6afffa9b4bbbfe7a0a96ffa0e5b8fad3183765
Vitaliy Gavrilov <vital.gavrilov1983@gmail.com>
2021-10-06 21:53:15 +0300

    Task_04 MainReflection

--- src/com/urise/webapp/MainReflection.java
+++ src/com/urise/webapp/MainReflection.java
@@ -1,7 +1,22 @@
 package com.urise.webapp;
 
+import com.urise.webapp.model.Resume;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class MainReflection {
     public static void main(String[] args) {
-
+        Resume r = new Resume();
+        Class resClass = r.getClass();
+        String resName = resClass.getName();
+        System.out.println("Имя класса: " + resClass.getName());
+        System.out.println("Поля класса: " + Arrays.toString(resClass.getDeclaredFields()));
+        System.out.println("Методы класса: ");
+        Method[] methods = resClass.getDeclaredMethods();
+        for (Method metod : methods) {
+            System.out.println(metod);
+        }
+        System.out.println("Аннотации класса: " + Arrays.toString(resClass.getAnnotations()));
     }
 }
