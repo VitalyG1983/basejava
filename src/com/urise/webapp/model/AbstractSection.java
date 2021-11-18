0b2936f5b38289c4dff2a664d372bd6eaa8ff8a2
Vitaliy Gavrilov <vital.gavrilov1983@gmail.com>
2021-11-18 22:25:45 +0300

    addd

--- src/com/urise/webapp/model/AbstractSection.java
+++ src/com/urise/webapp/model/AbstractSection.java
@@ -1,10 +1,12 @@
 package com.urise.webapp.model;
 
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
 
 public class AbstractSection {
 //    private String text;
 //    private List<String> sectionText;
+private final EnumMap<SectionType, AbstractSection> sections = new EnumMap<>(SectionType.class);
 
 }
