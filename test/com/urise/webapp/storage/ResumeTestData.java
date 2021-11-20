2eedb4b5413c946b12272804e6a30d48751678af
Vitaly Gavrilov <vital.gavrilov1983@gmail.com>
2021-11-20 22:06:36 +0300

    ACHIEVEMENT, QUALIFICATIONS list fixed

--- test/com/urise/webapp/storage/ResumeTestData.java
+++ test/com/urise/webapp/storage/ResumeTestData.java
@@ -21,32 +21,34 @@ public class ResumeTestData {
         sections.put(SectionType.PERSONAL, personal);
         sections.put(SectionType.OBJECTIVE, new TextSection("Аналитический склад ума, сильная логика, креативность, инициативность. Пурист кода и архитектуры."));
 
-        TextListSection achievmentList = new TextListSection();
-        achievmentList.getTextListSection().add("С 2013 года: разработка проектов \"Разработка Web приложения\",\"Java Enterprise\", \"Многомодульный maven. Многопоточность. XML (JAXB/StAX). Веб сервисы (JAX-RS/SOAP). Удаленное взаимодействие (JMS/AKKA)\". Организация онлайн стажировок и ведение проектов. Более 1000 выпускников.\n" +
-                "Реализация двухфакторной аутентификации для онлайн платформы управления проектами Wrike. Интеграция с Twilio, DuoSecurity, Google Authenticator, Jira, Zendesk.\n" +
-                "Налаживание процесса разработки и непрерывной интеграции ERP системы River BPM. Интеграция с 1С, Bonita BPM, CMIS, LDAP. Разработка приложения управления окружением на стеке: Scala/Play/Anorm/JQuery. Разработка SSO аутентификации и авторизации различных ERP модулей, интеграция CIFS/SMB java сервера.\n" +
-                "Реализация c нуля Rich Internet Application приложения на стеке технологий JPA, Spring, Spring-MVC, GWT, ExtGWT (GXT), Commet, HTML5, Highstock для алгоритмического трейдинга.\n" +
-                "Создание JavaEE фреймворка для отказоустойчивого взаимодействия слабо-связанных сервисов (SOA-base архитектура, JAX-WS, JMS, AS Glassfish). Сбор статистики сервисов и информации о состоянии через систему мониторинга Nagios. Реализация онлайн клиента для администрирования и мониторинга системы по JMX (Jython/ Django).\n" +
-                "Реализация протоколов по приему платежей всех основных платежных системы России (Cyberplat, Eport, Chronopay, Сбербанк), Белоруcсии(Erip, Osmp) и Никарагуа.");
-        sections.put(SectionType.ACHIEVEMENT, achievmentList);
+        TextListSection tls = new TextListSection();
+        List<String> achievSection = tls.getTextListSection();
+        achievSection.add("С 2013 года: разработка проектов \"Разработка Web приложения\",\"Java Enterprise\", \"Многомодульный maven. Многопоточность. XML (JAXB/StAX). Веб сервисы (JAX-RS/SOAP). Удаленное взаимодействие (JMS/AKKA)\". Организация онлайн стажировок и ведение проектов. Более 1000 выпускников.");
+        achievSection.add("Реализация двухфакторной аутентификации для онлайн платформы управления проектами Wrike. Интеграция с Twilio, DuoSecurity, Google Authenticator, Jira, Zendesk.");
+        achievSection.add("Налаживание процесса разработки и непрерывной интеграции ERP системы River BPM. Интеграция с 1С, Bonita BPM, CMIS, LDAP. Разработка приложения управления окружением на стеке: Scala/Play/Anorm/JQuery. Разработка SSO аутентификации и авторизации различных ERP модулей, интеграция CIFS/SMB java сервера.");
+        achievSection.add("Реализация c нуля Rich Internet Application приложения на стеке технологий JPA, Spring, Spring-MVC, GWT, ExtGWT (GXT), Commet, HTML5, Highstock для алгоритмического трейдинга.");
+        achievSection.add("Создание JavaEE фреймворка для отказоустойчивого взаимодействия слабо-связанных сервисов (SOA-base архитектура, JAX-WS, JMS, AS Glassfish). Сбор статистики сервисов и информации о состоянии через систему мониторинга Nagios. Реализация онлайн клиента для администрирования и мониторинга системы по JMX (Jython/ Django).");
+        achievSection.add("Реализация протоколов по приему платежей всех основных платежных системы России (Cyberplat, Eport, Chronopay, Сбербанк), Белоруcсии(Erip, Osmp) и Никарагуа.");
+        sections.put(SectionType.ACHIEVEMENT, tls);
 
-        TextListSection qualificationsList = new TextListSection();
-        qualificationsList.getTextListSection().add("JEE AS: GlassFish (v2.1, v3), OC4J, JBoss, Tomcat, Jetty, WebLogic, WSO2\n" +
-                "Version control: Subversion, Git, Mercury, ClearCase, Perforce\n" +
-                "DB: PostgreSQL(наследование, pgplsql, PL/Python), Redis (Jedis), H2, Oracle,\n" +
-                "MySQL, SQLite, MS SQL, HSQLDB\n" +
-                "Languages: Java, Scala, Python/Jython/PL-Python, JavaScript, Groovy,\n" +
-                "XML/XSD/XSLT, SQL, C/C++, Unix shell scripts,\n" +
-                "Java Frameworks: Java 8 (Time API, Streams), Guava, Java Executor, MyBatis, Spring (MVC, Security, Data, Clouds, Boot), JPA (Hibernate, EclipseLink), Guice, GWT(SmartGWT, ExtGWT/GXT), Vaadin, Jasperreports, Apache Commons, Eclipse SWT, JUnit, Selenium (htmlelements).\n" +
-                "Python: Django.\n" +
-                "JavaScript: jQuery, ExtJS, Bootstrap.js, underscore.js\n" +
-                "Scala: SBT, Play2, Specs2, Anorm, Spray, Akka\n" +
-                "Технологии: Servlet, JSP/JSTL, JAX-WS, REST, EJB, RMI, JMS, JavaMail, JAXB, StAX, SAX, DOM, XSLT, MDB, JMX, JDBC, JPA, JNDI, JAAS, SOAP, AJAX, Commet, HTML5, ESB, CMIS, BPMN2, LDAP, OAuth1, OAuth2, JWT.\n" +
-                "Инструменты: Maven + plugin development, Gradle, настройка Ngnix,\n" +
-                "администрирование Hudson/Jenkins, Ant + custom task, SoapUI, JPublisher, Flyway, Nagios, iReport, OpenCmis, Bonita, pgBouncer.\n" +
-                "Отличное знание и опыт применения концепций ООП, SOA, шаблонов проектрирования, архитектурных шаблонов, UML, функционального программирования\n" +
-                "Родной русский, английский \"upper intermediate\"");
-        sections.put(SectionType.QUALIFICATIONS, qualificationsList);
+        TextListSection tlsQ = new TextListSection();
+        List<String> qualSection = tlsQ.getTextListSection();
+        qualSection.add("JEE AS: GlassFish (v2.1, v3), OC4J, JBoss, Tomcat, Jetty, WebLogic, WSO2");
+        qualSection.add("Version control: Subversion, Git, Mercury, ClearCase, Perforce");
+        qualSection.add("DB: PostgreSQL(наследование, pgplsql, PL/Python), Redis (Jedis), H2, Oracle,");
+        qualSection.add("MySQL, SQLite, MS SQL, HSQLDB");
+        qualSection.add("Languages: Java, Scala, Python/Jython/PL-Python, JavaScript, Groovy,");
+        qualSection.add("XML/XSD/XSLT, SQL, C/C++, Unix shell scripts,");
+        qualSection.add("Java Frameworks: Java 8 (Time API, Streams), Guava, Java Executor, MyBatis, Spring (MVC, Security, Data, Clouds, Boot), JPA (Hibernate, EclipseLink), Guice, GWT(SmartGWT, ExtGWT/GXT), Vaadin, Jasperreports, Apache Commons, Eclipse SWT, JUnit, Selenium (htmlelements).");
+        qualSection.add("Python: Django.");
+        qualSection.add("JavaScript: jQuery, ExtJS, Bootstrap.js, underscore.js");
+        qualSection.add("Scala: SBT, Play2, Specs2, Anorm, Spray, Akka");
+        qualSection.add("Технологии: Servlet, JSP/JSTL, JAX-WS, REST, EJB, RMI, JMS, JavaMail, JAXB, StAX, SAX, DOM, XSLT, MDB, JMX, JDBC, JPA, JNDI, JAAS, SOAP, AJAX, Commet, HTML5, ESB, CMIS, BPMN2, LDAP, OAuth1, OAuth2, JWT.");
+        qualSection.add("Инструменты: Maven + plugin development, Gradle, настройка Ngnix,");
+        qualSection.add("администрирование Hudson/Jenkins, Ant + custom task, SoapUI, JPublisher, Flyway, Nagios, iReport, OpenCmis, Bonita, pgBouncer.");
+        qualSection.add("Отличное знание и опыт применения концепций ООП, SOA, шаблонов проектрирования, архитектурных шаблонов, UML, функционального программирования");
+        qualSection.add("Родной русский, английский \"upper intermediate\"");
+        sections.put(SectionType.QUALIFICATIONS, tlsQ);
 
 //////////////////////////////--------JobSection------//////////////////////////////////////////////
         JobEducationSection jobSection = new JobEducationSection();
@@ -118,18 +120,26 @@ public class ResumeTestData {
         for (EnumMap.Entry<ContactType, String> entry : contacts.entrySet()) {
             System.out.println(entry);
         }
-        if (sections.containsKey(SectionType.PERSONAL))
+        if (sections.containsKey(SectionType.PERSONAL)) {
             System.out.println("\n" + SectionType.PERSONAL.getTitle());
-        System.out.println(((TextSection) sections.get(SectionType.PERSONAL)).getTextSection());
-        if (sections.containsKey(SectionType.OBJECTIVE))
+            System.out.println(((TextSection) sections.get(SectionType.PERSONAL)).getTextSection());
+        }
+        if (sections.containsKey(SectionType.OBJECTIVE)) {
             System.out.println("\n" + SectionType.OBJECTIVE.getTitle());
-        System.out.println(((TextSection) sections.get(SectionType.OBJECTIVE)).getTextSection());
-        if (sections.containsKey(SectionType.ACHIEVEMENT))
+            System.out.println(((TextSection) sections.get(SectionType.OBJECTIVE)).getTextSection());
+        }
+        if (sections.containsKey(SectionType.ACHIEVEMENT)) {
             System.out.println("\n" + SectionType.ACHIEVEMENT.getTitle());
-        System.out.println(((TextListSection) sections.get(SectionType.ACHIEVEMENT)).getTextListSection());
-        if (sections.containsKey(SectionType.QUALIFICATIONS))
+            List<String> tls = ((TextListSection) sections.get(SectionType.ACHIEVEMENT)).getTextListSection();
+            for (String text : tls)
+                System.out.println("- " + text);
+        }
+        if (sections.containsKey(SectionType.QUALIFICATIONS)) {
             System.out.println("\n" + SectionType.QUALIFICATIONS.getTitle());
-        System.out.println(((TextListSection) sections.get(SectionType.QUALIFICATIONS)).getTextListSection());
+            List<String> tls = ((TextListSection) sections.get(SectionType.QUALIFICATIONS)).getTextListSection();
+            for (String text : tls)
+                System.out.println("- " + text);
+        }
         if (sections.containsKey(SectionType.EXPERIENCE))
             System.out.println("\n" + SectionType.EXPERIENCE.getTitle());
         List<JobEducationTextSection> jobSection = ((JobEducationSection) sections.get(SectionType.EXPERIENCE)).getJobEducationSection();
