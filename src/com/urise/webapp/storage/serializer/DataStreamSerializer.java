aad61a45988fc4b13ece486a40712d5243de131e
Vitaly Gavrilov <vital.gavrilov1983@gmail.com>
2021-12-20 23:29:51 +0300

    HW 10_Swith_fixed in doWrite()

--- src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
+++ src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
@@ -28,39 +28,39 @@ public class DataStreamSerializer implements Serialization {
             //////////////////        SectionType        ////////////////////////////////
             for (Map.Entry<SectionType, AbstractSection> entry : sections.entrySet()) {
                 dos.writeUTF(entry.getKey().getTitle());
-                TextSection textSection = entry.getValue() instanceof TextSection ? ((TextSection) entry.getValue()) : null;
-                TextListSection textListSection = entry.getValue() instanceof TextListSection ?
-                        ((TextListSection) entry.getValue()) : null;
-                OrganizationsSection organizations = entry.getValue() instanceof OrganizationsSection ?
-                        ((OrganizationsSection) entry.getValue()) : null;
-                if (textSection != null)
-                    dos.writeUTF(textSection.getText());
-                else if (textListSection != null) {
-                    dos.writeInt(textListSection.getListSection().size());
-                    for (String text : textListSection.getListSection())
-                        dos.writeUTF(text);
-                } else if (organizations != null) {
-                    dos.writeInt(organizations.getListOrganizations().size());
-                    for (Organization organization : organizations.getListOrganizations()) {
-                        dos.writeUTF(organization.getHomePage().getName());
-                        if (organization.getHomePage().getUrl() == null) {
-                            dos.writeUTF("null");
-                        } else dos.writeUTF(organization.getHomePage().getUrl());
-                        dos.writeInt(organization.getListExperience().size());
-                        for (Organization.Experience exp : organization.getListExperience()) {
-                            writeDate(dos, exp.getStartDate(), exp.getEndDate());
-                            if (exp.getTitle() == null)
+                switch (entry.getKey().getTitle()) {
+                    case "Личные качества", "Позиция" -> dos.writeUTF(((TextSection) entry.getValue()).getText());
+                    case "Достижения", "Квалификация" -> {
+                        dos.writeInt(((TextListSection) entry.getValue()).getListSection().size());
+                        for (String text : ((TextListSection) entry.getValue()).getListSection())
+                            dos.writeUTF(text);
+                    }
+                    case "Опыт работы", "Образование" -> {
+                        List<Organization> organizations = ((OrganizationsSection) entry.
+                                getValue()).getListOrganizations();
+                        dos.writeInt(organizations.size());
+                        for (Organization org : organizations) {
+                            dos.writeUTF(org.getHomePage().getName());
+                            if (org.getHomePage().getUrl() == null) {
                                 dos.writeUTF("null");
-                            else dos.writeUTF(exp.getTitle());
-                            if (exp.getDescription() == null)
-                                dos.writeUTF("null");
-                            else dos.writeUTF(exp.getDescription());
+                            } else dos.writeUTF(org.getHomePage().getUrl());
+                            dos.writeInt(org.getListExperience().size());
+                            for (Organization.Experience exp : org.getListExperience()) {
+                                writeDate(dos, exp.getStartDate(), exp.getEndDate());
+                                if (exp.getTitle() == null)
+                                    dos.writeUTF("null");
+                                else dos.writeUTF(exp.getTitle());
+                                if (exp.getDescription() == null)
+                                    dos.writeUTF("null");
+                                else dos.writeUTF(exp.getDescription());
+                            }
                         }
                     }
                 }
             }
         }
     }
+
 
     @Override
     public Resume doRead(InputStream is) throws IOException {
@@ -80,10 +80,10 @@ public class DataStreamSerializer implements Serialization {
                 switch (sectionTittle) {
                     case "Личные качества" -> sections.put(SectionType.PERSONAL, new TextSection(dis.readUTF()));
                     case "Позиция" -> sections.put(SectionType.OBJECTIVE, new TextSection(dis.readUTF()));
-                    case "Достижения" -> createTextListSection(dis, sections, SectionType.ACHIEVEMENT);
-                    case "Квалификация" -> createTextListSection(dis, sections, SectionType.QUALIFICATIONS);
-                    case "Опыт работы" -> createOrgSection(dis, sections, SectionType.EXPERIENCE);
-                    case "Образование" -> createOrgSection(dis, sections, SectionType.EDUCATION);
+                    case "Достижения" -> readTextListSection(dis, sections, SectionType.ACHIEVEMENT);
+                    case "Квалификация" -> readTextListSection(dis, sections, SectionType.QUALIFICATIONS);
+                    case "Опыт работы" -> readOrgSection(dis, sections, SectionType.EXPERIENCE);
+                    case "Образование" -> readOrgSection(dis, sections, SectionType.EDUCATION);
                 }
             }
             return resume;
@@ -99,7 +99,7 @@ public class DataStreamSerializer implements Serialization {
         return LocalDate.parse(dis.readUTF(), FORMATTER);
     }
 
-    private void createTextListSection(DataInputStream dis, Map<SectionType, AbstractSection> sections, SectionType secType) throws IOException {
+    private void readTextListSection(DataInputStream dis, Map<SectionType, AbstractSection> sections, SectionType secType) throws IOException {
         int size = dis.readInt();
         TextListSection tls = new TextListSection(new ArrayList<>());
         List<String> listString = tls.getListSection();
@@ -109,7 +109,7 @@ public class DataStreamSerializer implements Serialization {
         sections.put(secType, tls);
     }
 
-    private void createOrgSection(DataInputStream dis, Map<SectionType, AbstractSection> sections, SectionType secType) throws IOException {
+    private void readOrgSection(DataInputStream dis, Map<SectionType, AbstractSection> sections, SectionType secType) throws IOException {
         int size = dis.readInt();
         OrganizationsSection section = new OrganizationsSection(new ArrayList<>());
         for (int z = 0; z < size; z++) {
