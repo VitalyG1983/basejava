a820797d25f85d79ae5eb16567c507f70aaa08a6
Vitaliy Gavrilov <vital.gavrilov1983@gmail.com>
2022-01-04 11:25:45 +0300

    HW 10_FuncInterface ver_1.7

--- src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
+++ src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
@@ -1,6 +1,5 @@
 package com.urise.webapp.storage.serializer;
 
-import com.urise.webapp.exception.StorageException;
 import com.urise.webapp.model.*;
 
 import java.io.*;
@@ -19,60 +18,37 @@ public class DataStreamSerializer implements Serialization {
             Collection<Map.Entry<ContactType, String>> contCollection = new ArrayList<>(r.getContacts().entrySet());
             writeWithException(contCollection, dos,
                     (entry) -> {
-                        try {
-                            dos.writeUTF(entry.getKey().name());
-                            dos.writeUTF(entry.getValue());
-                        } catch (IOException e) {
-                            throw new StorageException("Error doWrite \"Contacts\" of resume in DataStreamSerializer", null, e);
-                        }
+                        dos.writeUTF(entry.getKey().name());
+                        dos.writeUTF(entry.getValue());
                     });
             Collection<Map.Entry<SectionType, AbstractSection>> sectionCollection = new ArrayList<>(r.getSections().entrySet());
             /////////////////////////        SectionType        ////////////////////////////////
             writeWithException(sectionCollection, dos, (entry) -> {
-                try {
-                    SectionType keyName = entry.getKey();
-                    dos.writeUTF(keyName.name());
-                    Object entryValue = entry.getValue();
-                    switch (keyName) {
-                        case PERSONAL, OBJECTIVE -> dos.writeUTF(((TextSection) entryValue).getText());
-                        case ACHIEVEMENT, QUALIFICATIONS -> {
-                            Collection<String> listSection = ((TextListSection) entryValue).getListSection();
-                            writeWithException(listSection, dos, (string) -> {
-                                try {
-                                    dos.writeUTF(string);
-                                } catch (IOException e) {
-                                    throw new StorageException("Error doWrite " + keyName + " of resume in DataStreamSerializer", null, e);
-                                }
-                            });
-
-                        }
-                        case EXPERIENCE, EDUCATION -> {
-                            Collection<Organization> organizations = ((OrganizationsSection) entryValue).getListOrganizations();
-                            writeWithException(organizations, dos, (org) -> {
-                                try {
-                                    Link homePage = org.getHomePage();
-                                    String url = homePage.getUrl();
-                                    dos.writeUTF(homePage.getName());
-                                    dos.writeUTF(url == null ? "null" : url);
-                                    Collection<Organization.Experience> listExperience = org.getListExperience();
-                                    writeWithException(listExperience, dos, (exp) -> {
-                                        try {
-                                            writeDate(dos, exp.getStartDate(), exp.getEndDate());
-                                            String title = exp.getTitle();
-                                            dos.writeUTF(title == null ? "null" : title);
-                                            dos.writeUTF(exp.getDescription() == null ? "null" : exp.getDescription());
-                                        } catch (IOException e) {
-                                            throw new StorageException("Error doWrite " + keyName + " of resume in DataStreamSerializer", null, e);
-                                        }
-                                    });
-                                } catch (IOException e) {
-                                    throw new StorageException("Error doWrite " + keyName + " of resume in DataStreamSerializer", null, e);
-                                }
-                            });
-                        }
+                SectionType keyName = entry.getKey();
+                dos.writeUTF(keyName.name());
+                Object entryValue = entry.getValue();
+                switch (keyName) {
+                    case PERSONAL, OBJECTIVE -> dos.writeUTF(((TextSection) entryValue).getText());
+                    case ACHIEVEMENT, QUALIFICATIONS -> {
+                        Collection<String> listSection = ((TextListSection) entryValue).getListSection();
+                        writeWithException(listSection, dos, (string) -> dos.writeUTF(string));
                     }
-                } catch (IOException e) {
-                    throw new StorageException("Error doWrite SectionType of resume in DataStreamSerializer", null, e);
+                    case EXPERIENCE, EDUCATION -> {
+                        Collection<Organization> organizations = ((OrganizationsSection) entryValue).getListOrganizations();
+                        writeWithException(organizations, dos, (org) -> {
+                            Link homePage = org.getHomePage();
+                            String url = homePage.getUrl();
+                            dos.writeUTF(homePage.getName());
+                            dos.writeUTF(url == null ? "null" : url);
+                            Collection<Organization.Experience> listExperience = org.getListExperience();
+                            writeWithException(listExperience, dos, (exp) -> {
+                                writeDate(dos, exp.getStartDate(), exp.getEndDate());
+                                String title = exp.getTitle();
+                                dos.writeUTF(title == null ? "null" : title);
+                                dos.writeUTF(exp.getDescription() == null ? "null" : exp.getDescription());
+                            });
+                        });
+                    }
                 }
             });
         }
@@ -85,12 +61,29 @@ public class DataStreamSerializer implements Serialization {
             String fullName = dis.readUTF();
             Resume resume = new Resume(uuid, fullName);
             Map<SectionType, AbstractSection> sections = resume.getSections();
-            int size = dis.readInt();
+            readWithException(dis,
+                    () -> {
+                        String value = dis.readUTF();
+                        resume.addContact(ContactType.valueOf(value), dis.readUTF());
+                    });
+           /* int size = dis.readInt();
             for (int i = 0; i < size; i++) {
                 String value = dis.readUTF();
                 resume.addContact(ContactType.valueOf(value), dis.readUTF());
-            }
-            int sizeSections = dis.readInt();
+            }*/
+            readWithException(dis,
+                    () -> {
+                        SectionType sectionType = SectionType.valueOf(dis.readUTF());
+                        switch (sectionType) {
+                            case PERSONAL, OBJECTIVE -> sections.put(sectionType.equals(SectionType.PERSONAL) ? SectionType.PERSONAL :
+                                    SectionType.OBJECTIVE, new TextSection(dis.readUTF()));
+                            case ACHIEVEMENT, QUALIFICATIONS -> readTextListSection(dis, sections, sectionType.equals(SectionType.ACHIEVEMENT) ?
+                                    SectionType.ACHIEVEMENT : SectionType.QUALIFICATIONS);
+                            case EXPERIENCE, EDUCATION -> readOrgSection(dis, sections, sectionType.equals(SectionType.EXPERIENCE) ?
+                                    SectionType.EXPERIENCE : SectionType.EDUCATION);
+                        }
+                    });
+          /*  int sizeSections = dis.readInt();
             for (int i = 0; i < sizeSections; i++) {
                 SectionType sectionType = SectionType.valueOf(dis.readUTF());
                 switch (sectionType) {
@@ -101,25 +94,38 @@ public class DataStreamSerializer implements Serialization {
                     case EXPERIENCE, EDUCATION -> readOrgSection(dis, sections, sectionType.equals(SectionType.EXPERIENCE) ?
                             SectionType.EXPERIENCE : SectionType.EDUCATION);
                 }
-            }
+            }*/
             return resume;
         }
     }
 
     @FunctionalInterface
-    public interface CustomConsumer<T> {
+    public interface CustomWriter<T> {
 
         void writeToDos(T t) throws IOException;
     }
 
+    @FunctionalInterface
+    public interface CustomReader {
+
+        void readFromDis() throws IOException;
+    }
+
     private <T> void writeWithException(Collection<T> entrySet, DataOutputStream dos,
-                                        CustomConsumer<T> customConsumer) throws IOException {
-        Objects.requireNonNull(customConsumer);
+                                        CustomWriter<T> customWriter) throws IOException {
         dos.writeInt(entrySet.size());
         for (T entry : entrySet) {
-            customConsumer.writeToDos(entry);
+            customWriter.writeToDos(entry);
         }
     }
+
+    private void readWithException(DataInputStream dis, CustomReader customReader) throws IOException {
+        int size = dis.readInt();
+        for (int i = 0; i < size; i++) {
+            customReader.readFromDis();
+        }
+    }
+
 
     private void writeDate(DataOutputStream dos, LocalDate start, LocalDate end) throws IOException {
         dos.writeUTF(FORMATTER.format(start));
