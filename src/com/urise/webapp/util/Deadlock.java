e1160117bed5e3a0ef28e7c056bcc53fbe148bd6
Vitaly Gavrilov <vital.gavrilov1983@gmail.com>
2022-01-05 19:42:03 +0300

    HW 11 DeadLock_Ver 1_4

--- src/com/urise/webapp/util/Deadlock.java
+++ src/com/urise/webapp/util/Deadlock.java
@@ -8,35 +8,26 @@ public class Deadlock {
         createThread(two, one);
     }
 
-    public static boolean objectOne(Object one) {
-        return one == Deadlock.one;
+    public static String checkOne(Object one) {
+        return one == Deadlock.one ? " one" : " two";
     }
 
     public static void createThread(Object one, Object two) {
         new Thread(() -> {
             try {
-                runThread(one, two);
+                System.out.println(Thread.currentThread().getName() + " try to block Object" + checkOne(one));
+                // Блокировка первого объекта
+                synchronized (one) {
+                    System.out.println(Thread.currentThread().getName() + " blocked Object" + checkOne(one));
+                    Thread.sleep(50);
+                    // Блокировка второго объекта
+                    synchronized (two) {
+                        System.out.println(Thread.currentThread().getName() + " blocked Object" + checkOne(one));
+                    }
+                }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }).start();
-    }
-
-    public static String printThread(String thread, boolean objectOne, boolean synchronizedFlag) {
-        String print = !synchronizedFlag ? (thread + " try to block Object") : (thread + " blocked Object");
-        return objectOne ? print + " one" : print + " two";
-    }
-
-    public static void runThread(Object one, Object two) throws InterruptedException {
-        System.out.println(printThread(Thread.currentThread().getName(), objectOne(one), false));
-        // Блокировка первого объекта
-        synchronized (one) {
-            System.out.println(printThread(Thread.currentThread().getName(), objectOne(one), true));
-            Thread.sleep(50);
-            // Блокировка второго объекта
-            synchronized (two) {
-                System.out.println(printThread(Thread.currentThread().getName(), objectOne(one), true));
-            }
-        }
     }
 }
