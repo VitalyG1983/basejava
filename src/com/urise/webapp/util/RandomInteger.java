7d5a12c9ee863da2568c7ea72054dfe9f0570e54
Vitaly Gavrilov <vital.gavrilov1983@gmail.com>
2022-01-09 14:38:02 +0300

    HW_12 Numbers_Ver 1_2

--- src/com/urise/webapp/util/RandomInteger.java
+++ src/com/urise/webapp/util/RandomInteger.java
@@ -4,44 +4,45 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class RandomInteger {
 
     public static void main(String[] args) {
         int[] values = new Random().ints(9, 1, 9).toArray();
-        System.out.println("Входной массив чисел для обоих заданий: "+ Arrays.toString(values));
-     //////////  1st homeWork  ///////////////////////
-        joinNumbers(values);
+        System.out.println("Входной массив чисел для обоих заданий: " + Arrays.toString(values));
+        //////////  1st homeWork  ///////////////////////
+        System.out.println(minValue(values));
 
-     /////////   2nd homeWork  ///////////////////////
+        /////////   2nd homeWork  ///////////////////////
         List<Integer> list = new ArrayList<>();
         Arrays.stream(values).forEach(list::add);
-        List<Integer> lis = oddOrEven(list);
+        // List<Integer> list = Arrays.stream(values).collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+        System.out.println(oddOrEven(list).toString());
     }
 
-    static void joinNumbers(int[] values) {
-        Function<int[], Integer> valueConverter = x -> {
+    static int minValue(int[] values) {
+        return Arrays.stream(values).distinct().sorted().reduce(0, (result, x) -> 10 * result + x);
+        /*  Function<int[], Integer> valueConverter = x -> {
             int result = 0;
             for (int j : x) {
                 result *= 10;
                 result += j;
             }
             return result;
-        };
-        System.out.println(valueConverter.apply(Arrays.stream(values).sorted().distinct().toArray()));
+        };*/
     }
 
     static List<Integer> oddOrEven(List<Integer> integers) {
-        List<Integer> finalList;
+        int sum = integers.stream().reduce(0, Integer::sum);
+        return integers.stream().filter(x -> (sum % 2 == 0) == (x % 2 != 0)).collect(Collectors.toList());
+       /* List<Integer> finalList;
         Function<List<Integer>, List<Integer>> sumFounder = list -> {
             int result = 0;
             for (int elem : list) {
                 result += elem;
             }
-                if (result % 2 == 0) {
+            if (result % 2 == 0) {
                 System.out.println("Сумма чисел четная, убираем четные числа");
                 return doFilter(list, x -> x % 2 != 0);
             } else {
@@ -49,12 +50,10 @@ public class RandomInteger {
                 return doFilter(list, x -> x % 2 == 0);
             }
         };
-        finalList = sumFounder.apply(integers);
-        System.out.println(finalList);
-        return finalList;
+        finalList = sumFounder.apply(integers);*/
     }
 
-    static List<Integer> doFilter(List<Integer> list, Predicate<? super Integer>  predicate ){
+    /*static List<Integer> doFilter(List<Integer> list, Predicate<? super Integer> predicate) {
         return list.stream().filter(predicate).collect(Collectors.toList());
-    }
+    }*/
 }
