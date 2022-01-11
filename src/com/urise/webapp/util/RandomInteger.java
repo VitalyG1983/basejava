ce58648bfa878e4d30b23a42120b77d310d491a8
Vitaly Gavrilov <vital.gavrilov1983@gmail.com>
2022-01-11 21:35:07 +0300

    HW_12 Numbers_Ver 1_4

--- src/com/urise/webapp/util/RandomInteger.java
+++ src/com/urise/webapp/util/RandomInteger.java
@@ -4,13 +4,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class RandomInteger {
 
     public static void main(String[] args) {
-        int[] values = new Random().ints(9, 1, 9).toArray();
+        int[] values = new Random().ints(3, 1, 9).toArray();
         System.out.println("Входной массив чисел для обоих заданий: " + Arrays.toString(values));
         //////////  1st homeWork  ///////////////////////
         System.out.println(minValue(values));
@@ -35,8 +34,8 @@ public class RandomInteger {
     }
 
     static List<Integer> oddOrEven(List<Integer> integers) {
-        int sum = integers.stream().reduce(0, Integer::sum);
-        return (sum % 2 == 0) ? doFilter(integers, x -> x % 2 != 0) : doFilter(integers, x -> x % 2 == 0);
+        boolean remainsZero = (integers.stream().reduce(0, Integer::sum)) % 2 == 0;
+        return integers.stream().filter(x -> remainsZero ? (x % 2) > 0 : x % 2 == 0).collect(Collectors.toList());
 
        /* List<Integer> finalList;
         Function<List<Integer>, List<Integer>> sumFounder = list -> {
@@ -45,17 +44,17 @@ public class RandomInteger {
                 result += elem;
             }
             if (result % 2 == 0) {
-                System.out.println("Сумма чисел четная, убираем четные числа");
+                System.out.println("Сумма чисел четная, оставляем нечетные числа");
                 return doFilter(list, x -> x % 2 != 0);
             } else {
-                System.out.println("Сумма чисел нечетная, убираем нечетные числа");
+                System.out.println("Сумма чисел нечетная, оставляем четные числа");
                 return doFilter(list, x -> x % 2 == 0);
             }
         };
         finalList = sumFounder.apply(integers);*/
     }
 
-    static List<Integer> doFilter(List<Integer> list, Predicate<? super Integer> predicate) {
+    /*static List<Integer> doFilter(List<Integer> list, Predicate<? super Integer> predicate) {
         return list.stream().filter(predicate).collect(Collectors.toList());
-    }
+    }*/
 }
