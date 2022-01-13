73e9b63d6c0bc51d44992f60bed8f02ac842f1aa
Vitaliy Gavrilov <vital.gavrilov1983@gmail.com>
2022-01-13 03:08:31 +0300

    Lesson 13_22

--- src/com/urise/webapp/util/Config.java
+++ src/com/urise/webapp/util/Config.java
@@ -7,8 +7,8 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
+    private static final File PROPS = new File("config\\resumes.properties");
     private static final Config INSTANCE = new Config();
-    protected static final File PROPS = new File("./config/resumes.properties");
     private Properties props = new Properties();
     private  File storageDir;
 
@@ -19,7 +19,7 @@ public class Config {
     private Config() {
         try (InputStream is = new FileInputStream(PROPS)) {
             props.load(is);
-            storageDir = new File(props.getProperty("storage_dir"));
+            storageDir = new File(props.getProperty("storage.dir"));
         } catch (IOException e) {
             throw new IllegalStateException("Invalid config file" + PROPS.getAbsolutePath());
         }
